a8737c87fc23d06104255cb31c167ec38dde6378
Andreas1993 <adpo12@student.aau.dk>
2016-03-02 22:18:00 +0100

    dato 02-03-2016

--- app/src/main/java/com/example/andreasp/myapp/MainActivity.java
+++ app/src/main/java/com/example/andreasp/myapp/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.andreasp.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.EditText;
 
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    public void clickMenu(View view) {
+        Intent intent = new Intent(MainActivity.this, menuActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
