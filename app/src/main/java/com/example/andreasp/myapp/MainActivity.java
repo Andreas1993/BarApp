7a6cf6de969032ec970ee92c0a2aba0cf4a7f86f
Andreas1993 <adpo12@student.aau.dk>
2016-03-02 22:48:03 +0100

    dato 02-03-2016 0.0.1b

--- app/src/main/java/com/example/andreasp/myapp/MainActivity.java
+++ app/src/main/java/com/example/andreasp/myapp/MainActivity.java
@@ -5,15 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
-    public void clickMenu(View view) {
-        Intent intent = new Intent(MainActivity.this, menuActivity.class);
-        startActivity(intent);
+    public void clickButton(View view) {
+        switch(view.getId()){
+            case R.id.menuButton:
+                Intent menuIntent = new Intent(MainActivity.this, menuActivity.class);
+                startActivity(menuIntent);
+                break;
+            case  R.id.hoursButton:
+                Intent hoursIntent = new Intent(MainActivity.this, openingHours.class);
+                startActivity(hoursIntent);
+                break;
+            case R.id.eventsButton:
+                Intent eventsIntent = new Intent(MainActivity.this, events.class);
+                startActivity(eventsIntent);
+                break;
+        }
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
