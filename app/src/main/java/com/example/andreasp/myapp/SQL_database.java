a68ed89f3588d1e7377f82e8af259b6010c4d174
Andreas1993 <adpo12@student.aau.dk>
2016-04-06 20:41:57 +0200

    Can load events from database with cursor

--- app/src/main/java/com/example/andreasp/myapp/SQL_database.java
+++ app/src/main/java/com/example/andreasp/myapp/SQL_database.java
@@ -16,13 +16,18 @@ public class SQL_database extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public final String TABLE_PARENT = "Types";
     public final String TABLE_CHILD = "Drinks";
+    public final String EVENT_TABLE = "Events";
+    public final String EVENT_ID = "_id";
     public final String PARENT_ID = "_id";
     public final String CHILD_ID = "_id";
     public final String DRINKS_IMAGE = "Image";
+    public final String EVENT_NAME = "Name";
     public final String PARENT_NAME = "Name";
     public final String CHILD_NAME = "Name";
+    public final String EVENT_PARTAKERS = "Partaking";
     public final String DRINKS_PRICE = "Price";
-
+    public final String EVENT_DRINK = "Drink";
+    public final String EVENT_DATE = "Date";
 
     // CREATE TABLE Drinks ();
     public SQL_database(Context context) {
@@ -35,8 +40,11 @@ public class SQL_database extends SQLiteOpenHelper {
                 " INTEGER PRIMARY KEY AUTOINCREMENT, " + DRINKS_IMAGE + " INTEGER, " + PARENT_NAME + " TEXT);";
         String ChildQuery = "CREATE TABLE " + TABLE_CHILD + " (" + CHILD_ID +
                 " INTEGER PRIMARY KEY AUTOINCREMENT, " + CHILD_NAME + " TEXT, " + DRINKS_PRICE + " TEXT); ";
+        String EventQuery = "CREATE TABLE " + EVENT_TABLE + " (" + EVENT_ID +
+                " INTEGER PRIMARY KEY AUTOINCREMENT, " + EVENT_NAME + " TEXT, " + EVENT_DRINK + " TEXT, " + EVENT_DATE + " TEXT, " + EVENT_PARTAKERS + " TEXT); ";
         db.execSQL(ParentQuery);
         db.execSQL(ChildQuery);
+        db.execSQL(EventQuery);
 
 
     }
@@ -45,6 +53,7 @@ public class SQL_database extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + PARENT_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + CHILD_NAME);
+        db.execSQL("DROP TABLE IF EXISTS" + EVENT_NAME);
         onCreate(db);
     }
 
--- app/src/main/java/com/example/andreasp/myapp/events.java
+++ app/src/main/java/com/example/andreasp/myapp/events.java
@@ -1,13 +1,58 @@
 package com.example.andreasp.myapp;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class events extends AppCompatActivity {
+
+    SQL_database myhelper;
+    SQLiteDatabase db;
+
+    public void CreateEvent(View view) {
+        Intent EventIntent = new Intent(events.this, event_creator.class);
+        startActivity(EventIntent);
+        }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        // TodoDatabaseHandler is a SQLiteOpenHelper class connecting to SQLite
+        myhelper = new SQL_database(this);
+        // Get access to the underlying writeable database
+        db = myhelper.getWritableDatabase();
+        // Query for items from the database and get a cursor back
+        Cursor todoCursor = db.rawQuery("SELECT  * FROM Events", null);
+
+        // Find ListView to populate
+        ListView Events = (ListView) findViewById(R.id.ComingEvents);
+        // Setup cursor adapter using cursor from last step
+        TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, todoCursor, 0);
+        // Attach cursor adapter to the ListView
+        Events.setAdapter(todoAdapter);
+
+        // Switch to new cursor and update contents of ListView
+
     }
+
+
+
 }
+
+
