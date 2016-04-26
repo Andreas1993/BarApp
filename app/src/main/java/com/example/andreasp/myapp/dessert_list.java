d7d2096e0dda35d9f2caace670df1c148b99955a
Andreas1993 <adpo12@student.aau.dk>
2016-04-26 11:57:41 +0200

    Now with music player buttons

--- app/src/main/java/com/example/andreasp/myapp/dessert_list.java
+++ app/src/main/java/com/example/andreasp/myapp/dessert_list.java
@@ -3,15 +3,19 @@ package com.example.andreasp.myapp;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class dessert_list extends AppCompatActivity {
 
+    private OrientationEventListener LandscapeChecker;
     dessertFragment dessertFrag;
+    int orientationValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +27,31 @@ public class dessert_list extends AppCompatActivity {
         fragmentTransaction.add(R.id.dessertList, dessertFrag, "myDessert");
         fragmentTransaction.commit();
 
+
+//        LandscapeChecker = new OrientationEventListener(getApplicationContext()) {
+//            @Override
+//            public void onOrientationChanged(int arg0) {
+//                orientationValue = arg0;
+//
+//                if ((orientationValue> 80 && orientationValue<100) || (orientationValue>260 && orientationValue<280)){
+//                    setContentView(R.layout.landscape_fragments);
+//                }
+//                if(orientationValue < 0){   // The phone lies on the table
+//
+//                }
+//            }
+//
+//        };
+//        if (LandscapeChecker.canDetectOrientation()){
+//            LandscapeChecker.enable();
+//        }
+
+
     }
+
+
+
+
 
 
 
