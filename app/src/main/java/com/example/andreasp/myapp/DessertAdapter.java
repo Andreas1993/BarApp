97f1360e74974ee0adedc335b31b147fcca42890
Andreas1993 <adpo12@student.aau.dk>
2016-04-26 21:18:36 +0200

    Working on google maps implementation

---
+++ app/src/main/java/com/example/andreasp/myapp/DessertAdapter.java
@@ -0,0 +1,61 @@
+package com.example.andreasp.myapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Andreasp on 26-04-2016.
+ */
+public class DessertAdapter extends BaseAdapter {
+
+    String[] data;
+    Context context;
+    private LayoutInflater dessertInflater;
+    TextView dessertText;
+
+
+
+
+    public DessertAdapter(String[] data, Context context){
+        super();
+        this.data = data;
+        this.context = context;
+        dessertInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView == null) {
+            convertView = dessertInflater.inflate(R.layout.activity_dessert_list, parent, false);
+        }
+        dessertText = (TextView)convertView.findViewById(R.id.dessertListText);
+
+        dessertText.setText(data[position]);
+
+        return convertView;
+    }
+
+
+}
--- app/src/main/java/com/example/andreasp/myapp/dessert_list.java
+++ app/src/main/java/com/example/andreasp/myapp/dessert_list.java
@@ -3,6 +3,7 @@ package com.example.andreasp.myapp;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,18 +11,29 @@ import android.view.LayoutInflater;
 import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class dessert_list extends AppCompatActivity {
 
     private OrientationEventListener LandscapeChecker;
     dessertFragment dessertFrag;
     int orientationValue;
+    ListView dessertList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dessert_list);
 
-        dessertFrag =  new dessertFragment();
+        String[] dessertArray = {"Strawberry icecream", "Pancakes", "Brownie", "Cheesecake"};
+
+        dessertList = (ListView) findViewById(R.id.dessertListView);
+        dessertList.setAdapter(new DessertAdapter(dessertArray, dessert_list.this));
+
+        dessertFrag = new dessertFragment();
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.dessertList, dessertFrag, "myDessert");
@@ -48,11 +60,6 @@ public class dessert_list extends AppCompatActivity {
 
 
     }
-
-
-
-
-
 
 
 }
